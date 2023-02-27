f03a632e26e16e5e222e28571b61281331714595
Mrk197 <mirkaalamilla97@gmail.com>
2023-02-26 23:44:53 -0600

    moved

--- src/components/Error/Error.jsx
+++
@@ -1,9 +0,0 @@
-import image from "../../assets/error404.png"
-
-export default function Error() {
-    return <div style={{color:'white'}}>
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <img src={image} />
-    </div>
-}
